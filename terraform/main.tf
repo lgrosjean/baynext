6d2816fcdf91173546c142cc907d48751407f722
Léo Grosjean <leo.grosjean@live.fr>
2025-07-04 22:31:17 +0200

    fix(terraform): format alignment and improve readability in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,15 +11,15 @@ locals {
 # Activate APIs for the project
 resource "google_project_service" "services" {
   for_each = toset(local.apis)
-  project = var.project_id
-  service = each.value
+  project  = var.project_id
+  service  = each.value
 }
 
 # Create a Docker repository in Artifact Registry
 # This repository will be used to store Docker images
 resource "google_artifact_registry_repository" "docker_repo" {
-  depends_on = [ google_project_service.services ]
-  project = var.project_id
+  depends_on    = [google_project_service.services]
+  project       = var.project_id
   location      = "europe-west1"
   repository_id = local.repository_id
   format        = "DOCKER"
