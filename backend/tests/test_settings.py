9bcfe741428bbc9079e8c02980c8d92bf66ef6c8
Léo Grosjean <leo.grosjean@live.fr>
2025-07-05 12:13:31 +0200

    feat: add dockerfile specification to hadolint action and include pytest-env dependency in project

--- backend/tests/test_settings.py
+++ backend/tests/test_settings.py
@@ -3,10 +3,18 @@ from app.core.settings import Settings
 
 def test_settings_initialization():
     # Test valid initialization
-    settings = Settings(ml_api_secret_api_key="test_secret_key")
+    settings = Settings(
+        ml_api_secret_api_key="test_secret_key",
+        database_url="postgresql://user:password@host:port/dbname",
+        blob_read_write_token="blob_read_write_token",
+    )
     assert settings.app_name == "Baynext API"
-    assert settings.root_path == "/api"
     assert settings.ml_api_secret_api_key.get_secret_value() == "test_secret_key"
+    assert (
+        settings.database_url.get_secret_value()
+        == "postgresql://user:password@host:port/dbname"
+    )
+    assert settings.blob_read_write_token.get_secret_value() == "blob_read_write_token"
 
 
 def test_settings_env_file_loading(monkeypatch):
