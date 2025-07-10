dee0c40eff72a0182f463b98e814d5240ae9447e
Léo Grosjean <leo.grosjean@live.fr>
2025-07-10 21:13:56 +0200

    fix(auth): refactor NextAuth setup to include  types

--- frontend/apps/console/src/auth.ts
+++ frontend/apps/console/src/auth.ts
@@ -1,7 +1,7 @@
 
 // https://dev.to/jamescroissant/user-authentication-with-authjs-in-nextjs-app-router-424k
 
-import NextAuth, { type DefaultSession } from "next-auth"
+import NextAuth, { type DefaultSession,  NextAuthResult } from "next-auth"
 import GitHub from "next-auth/providers/github"
 import * as jose from 'jose'
 
@@ -26,7 +26,7 @@ declare module "next-auth" {
   }
 }
  
-export const { auth, handlers, signIn, signOut } = NextAuth({
+const authOptions = NextAuth({
   session: { 
     strategy: "jwt",
     maxAge: 30 * 24 * 60 * 60, // 30 days
@@ -39,38 +39,44 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
     }),
-    ],
-    // https://authjs.dev/reference/nextjs#callbacks
-    callbacks: {
-      //  By default, the `id` property does not exist on `token` or `session`. See the [TypeScript](https://authjs.dev/getting-started/typescript) on how to add it.
-      async jwt({ token, user, account }) {
-        if (user) { // User is available during sign-in
-          token.id = user.id
-        }
-        return token
-      },
-      // https://authjs.dev/guides/extending-the-session#with-jwt
-      // This callback is called whenever a session is checked.
-      async session({ session, token, user }) {
-        if (token) {
-          session.user.id = token.id as string
-          
-          if (!process.env.AUTH_SECRET) throw new Error('JWT secret not found')
-          const secret = jose.base64url.decode(process.env.AUTH_SECRET)
-          session.token = await new jose.SignJWT(token)
-            .setProtectedHeader({ alg: 'HS256' })
-            .sign(secret)
-        }
-        return session
-      },
-      authorized({ auth, request: { nextUrl } }) {
-        const isLoggedIn = !!auth?.user;
-        const isOnDashboard = nextUrl.pathname.startsWith('/app');
-        if (isOnDashboard) {
-          if (isLoggedIn) return true;
-          return false; // Redirect unauthenticated users to login page
-        }
-        return true;
+  ],
+  // https://authjs.dev/reference/nextjs#callbacks
+  callbacks: {
+    //  By default, the `id` property does not exist on `token` or `session`. See the [TypeScript](https://authjs.dev/getting-started/typescript) on how to add it.
+    async jwt({ token, user, account }) {
+      if (user) { // User is available during sign-in
+        token.id = user.id
       }
+      return token
     },
-  })
+    // https://authjs.dev/guides/extending-the-session#with-jwt
+    // This callback is called whenever a session is checked.
+    async session({ session, token, user }) {
+      if (token) {
+        session.user.id = token.id as string
+        
+        if (!process.env.AUTH_SECRET) throw new Error('JWT secret not found')
+        const secret = jose.base64url.decode(process.env.AUTH_SECRET)
+        session.token = await new jose.SignJWT(token)
+          .setProtectedHeader({ alg: 'HS256' })
+          .sign(secret)
+      }
+      return session
+    },
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+      const isOnDashboard = nextUrl.pathname.startsWith('/app');
+      if (isOnDashboard) {
+        if (isLoggedIn) return true;
+        return false; // Redirect unauthenticated users to login page
+      }
+      return true;
+    }
+  },
+});
+
+// https://github.com/nextauthjs/next-auth/discussions/9950
+export const handlers: NextAuthResult['handlers'] = authOptions.handlers;
+export const auth: NextAuthResult['auth'] = authOptions.auth;
+export const signIn: NextAuthResult['signIn'] = authOptions.signIn;
+export const signOut: NextAuthResult['signOut'] = authOptions.signOut;
